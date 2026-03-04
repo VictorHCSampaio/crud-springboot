5deedca1151c0a17cb05c0c70202ee811c2ca8f2
suino <sampaiov917@gmail.com>
2026-03-03 23:36:49 -0300

    Mudando RequestMapping

--- src/main/java/umc/devapp/crud_produtos/controller/ProdutoController.java
+++ src/main/java/umc/devapp/crud_produtos/controller/ProdutoController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/produto")
+@RequestMapping("/fodase")
 @CrossOrigin(origins = "*")
 public class ProdutoController {
 
